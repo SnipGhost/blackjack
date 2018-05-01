20733aa9521a01cc143d8c1230010dedf475e638
SnipGhost <snipghost@list.ru>
2018-05-01 19:48:13 +0300

    Added comments to config

--- etc/config.php
+++ etc/config.php
@@ -8,12 +8,15 @@ if (DEBUG) {
 }
 
 // Адрес, по которому мы должны получать index нашего сайта
+// Например URL:    http://localhost/~snipghost/blackjack/
+// Тогда BASE_URI:  ~snipghost/blackjack/
 define('BASE_URI', '~snipghost/blackjack/');
 
 // Версия, для удобства написания
 define('VERSION', 'v0.1');
 
 // Файл с конфиденциальными настройками БД
+// Он в репозиторий НИКОГДА не коммитится (добавлен в .gitignore)
 include(ROOT.'etc/passwords.php');
 // define(HOSTNAME, "");
 // define(USERNAME, "");
