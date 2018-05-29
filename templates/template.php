d7c1344064eb3135d55310f9a7fca6af4fbbbbfa
SnipGhost <snipghost@list.ru>
2018-05-29 17:46:05 +0300

    More adaptive styles and namefixes

--- templates/template.php
+++ templates/template.php
@@ -29,40 +29,43 @@
 				</div>
 			</div>
 		</div>
-		<div class="content-box">
-			<div class="content-box-title">
-				О Сайте
-			</div>
-			<div class="ab-block">
-				<div class="ab-pic">
-					<img src=img/main-1.png>
+		<div id="block-about">
+			<div class="content-box">
+				<div class="content-box-title">О Сайте</div>
+				<div class="ab-box">
+					<div class="ab-pic">
+						<img src=img/main-1.png>
+					</div>
+					<div class="ab-text">
+						<div class="ab-title">Описание</div>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+						</p>
+					</div>
 				</div>
-				<div class="ab-text">
-					<div class="ab-title">Описание</div>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-					</p>
-				</div>
-			</div>
-			<div class="ab-block">
-				<div class="ab-pic">
-					<img src=img/main-2.png>
-				</div>
-				<div class="ab-text">
-					<div class="ab-title">Наши плюсы</div>
-					<p>
-					Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.
-					</p>
+				<div class="ab-box">
+					<div class="ab-pic">
+						<img src=img/main-2.png>
+					</div>
+					<div class="ab-text">
+						<div class="ab-title">Наши плюсы</div>
+						<p>
+						Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
-		<!-- <div class="offers">
-			<div class="offers-allin-block">
-				<div class="title">
-					Наши предложения
+		<div id="block-offers">
+			<div class="content-box">
+				<div class="content-box-title">Наши предложения</div>
+				<div class="offers-wrap">
+					<div id="kik" class="offer"></div>
+					<div id="opi" class="offer"></div>
+					<div id="smt" class="offer"></div>
 				</div>
 			</div>
-		</div> -->
+		</div>
 	</div>
 	<div class="footer">
 		<div class="footer-content">
