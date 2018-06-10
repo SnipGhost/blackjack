29fab4a76c0c2043bb9fdcf3f9d6998ecfbbc59f
SnipGhost <snipghost@list.ru>
2018-06-11 01:05:01 +0300

    Remaked code style in reg

--- views/reg/RegEnd.php
+++ views/reg/RegEnd.php
@@ -1,21 +1,16 @@
-<!--<div class="box">
-	<?php
-        echo 'Пользователь ', $_POST['username'], ' успешно добавлен';
-    ?>
-</div> -->
 <form action="" method="POST">
-    				<div class = "reg-block">
-                        <div class="reg-end">
-                            <?php
-                                echo 'Пользователь ', $_POST['email'], ' успешно добавлен';
-                            ?>
-                        </div>
-					</div>
-					<div class="buttons reg-endb">
-						<a href="/<?=BASE_URI?>">
-							<div class = "button-back reg-end">
-								Назад
-							</div>
-						</a>
-					</div>
+	<div class = "reg-block">
+		<div class="reg-end">
+			<?php
+				echo 'Пользователь ', $_POST['email'], ' успешно добавлен';
+			?>
+		</div>
+	</div>
+	<div class="buttons reg-endb">
+		<a href="/<?=BASE_URI?>">
+			<div class = "button-back reg-end">
+				Назад
+			</div>
+		</a>
+	</div>
 </form>
--- views/reg/RegForm.php
+++ views/reg/RegForm.php
@@ -1,38 +1,36 @@
 <form action="" method="POST">
-					<div class = "reg-block">
-						<div>
-							<div class="line title">
-								Регистрация
-								<div class = "semititle">
-									Создание нового пользователя
-								</div>
-							</div>
-							<div class="line">
-								<span class="orange"><?=$msg?></span>
-							</div>
-							<div class="line">
-								<input type="email" name="email" placeholder="Электронная почта"  required/>
-							</div>
-							
-							<div class="line">
-								<input type="password" name="password" placeholder="Пароль" required/>
-							</div>
+	<div class = "reg-block">
+		<div>
+			<div class="line title">
+				Регистрация
+				<div class = "semititle">
+					Создание нового пользователя
+				</div>
+			</div>
+			<div class="line">
+				<span class="orange"><?=$msg?></span>
+			</div>
+			<div class="line">
+				<input type="email" name="email" placeholder="Электронная почта"  required/>
+			</div>
+			
+			<div class="line">
+				<input type="password" name="password" placeholder="Пароль" required/>
+			</div>
 
-							<div class="line">
-								<input type="password" name="retype"placeholder="Повторите пароль" required/>
-							</div>
-
-							
-						</div>
-					</div>
-					<div class="buttons">
-						<a href="/<?=BASE_URI?>">
-							<div class = "button-back">
-								Назад
-							</div>
-						</a>
-							<button type="submit" name="reg" class = "button-reg">
-								Регистрация
-							</button>
-					</div>
+			<div class="line">
+				<input type="password" name="retype"placeholder="Повторите пароль" required/>
+			</div>
+		</div>
+	</div>
+	<div class="buttons">
+		<a href="/<?=BASE_URI?>">
+			<div class = "button-back">
+				Назад
+			</div>
+		</a>
+			<button type="submit" name="reg" class = "button-reg">
+				Регистрация
+			</button>
+	</div>
 </form>
